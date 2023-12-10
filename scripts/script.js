6889813175e303be7a71772ab5fa6d40908add8a
AlexX2now <145762129+AlexX2now@users.noreply.github.com>
2023-12-11 03:08:20 +0500

    Создание основ

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function hoverOnImgF(){
+    document.querySelector('.first').classList.add("visible");
+}
+function hoverOffImgF(){
+    document.querySelector('.first').classList.remove("visible");
+}
+
+function hoverOnImgS(){
+    document.querySelector('.second').classList.add("visible");
+}
+function hoverOffImgS(){
+    document.querySelector('.second').classList.remove("visible");
+}
