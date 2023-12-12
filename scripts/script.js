88161af187ad44c456fdfb95d8e769889205919e
AlexX2now <145762129+AlexX2now@users.noreply.github.com>
2023-12-12 15:59:44 +0500

    Дополнение мутаций и тесты

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//first mut
 function hoverOnImgF(){
     document.querySelector('.first').classList.add("visible");
 }
@@ -5,9 +6,26 @@ function hoverOffImgF(){
     document.querySelector('.first').classList.remove("visible");
 }
 
+//second mut
 function hoverOnImgS(){
     document.querySelector('.second').classList.add("visible");
 }
 function hoverOffImgS(){
     document.querySelector('.second').classList.remove("visible");
+}
+
+//third mut
+function hoverOnImgT(){
+    document.querySelector('.third').classList.add("visible");
+}
+function hoverOffImgT(){
+    document.querySelector('.third').classList.remove("visible");
+}
+
+//four mut
+function hoverOnImgI(){
+    document.querySelector('.four').classList.add("visible");
+}
+function hoverOffImgI(){
+    document.querySelector('.four').classList.remove("visible");
 }
