15735d1987c83dc35f2f3c7a281df9b184bf5ee2
AlexX2now <145762129+AlexX2now@users.noreply.github.com>
2024-07-26 22:45:42 +0500

    Полная перерработка

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,75 @@ switch (name){
     case "impact":
         document.querySelector('.four').classList.add("visible");
     break;
+    case "bludlust":
+        document.querySelector('.five').classList.add("visible");
+    break;
+    case "boltmarrow":
+        document.querySelector('.six').classList.add("visible");
+    break;
+    case "muscles":
+        document.querySelector('.seven').classList.add("visible");
+    break;
+    case "vens":
+        document.querySelector('.eight').classList.add("visible");
+    break;
+    case "eiforia":
+        document.querySelector('.nine').classList.add("visible");
+    break;
+    case "eagles":
+        document.querySelector('.ten').classList.add("visible");
+    break;
+    case "gamma":
+        document.querySelector('.elevn').classList.add("visible");
+    break;
+    case "heart":
+        document.querySelector('.twelwe').classList.add("visible");
+    break;
+    case "longarms":
+        document.querySelector('.thtn').classList.add("visible");
+    break;
+    case "laserbr":
+        document.querySelector('.fotn').classList.add("visible");
+    break;
+    case "luckyshot":
+        document.querySelector('.fitn').classList.add("visible");
+    break;
+    case "lastwish":
+        document.querySelector('.sixtn').classList.add("visible");
+    break;
+    case "openmind":
+        document.querySelector('.sevtn').classList.add("visible");
+    break;
+    case "patience":
+        document.querySelector('.eitn').classList.add("visible");
+    break;
+    case "pluto":
+        document.querySelector('.ninetn').classList.add("visible");
+    break;
+    case "rabbitpaw":
+        document.querySelector('.twotn').classList.add("visible");
+    break;
+    case "glands":
+        document.querySelector('.onetwotn').classList.add("visible");
+    break;
+    case "stomath":
+        document.querySelector('.twotwotn').classList.add("visible");
+    break;
+    case "face":
+        document.querySelector('.thrtwotn').classList.add("visible");
+    break;
+    case "shouldrs":
+        document.querySelector('.fourtwotn').classList.add("visible");
+    break;
+    case "sese":
+        document.querySelector('.fivetwotn').classList.add("visible");
+    break;
+    case "teeths":
+        document.querySelector('.sixtwotn').classList.add("visible");
+    break;
+    case "stress":
+        document.querySelector('.seventntwo').classList.add("visible");
+    break;
 }
 }
 
@@ -21,4 +90,32 @@ function closeall(){
     document.querySelector('.second').classList.remove("visible");
     document.querySelector('.third').classList.remove("visible");
     document.querySelector('.four').classList.remove("visible");
+    document.querySelector('.five').classList.remove("visible");
+    document.querySelector('.six').classList.remove("visible");
+    document.querySelector('.seven').classList.remove("visible");
+    document.querySelector('.eight').classList.remove("visible");
+    document.querySelector('.nine').classList.remove("visible");
+    document.querySelector('.ten').classList.remove("visible");
+    document.querySelector('.elevn').classList.remove("visible");
+    document.querySelector('.twelwe').classList.remove("visible");
+    document.querySelector('.thtn').classList.remove("visible");
+    document.querySelector('.fotn').classList.remove("visible");
+    document.querySelector('.fitn').classList.remove("visible");
+    document.querySelector('.sixtn').classList.remove("visible");
+    document.querySelector('.sevtn').classList.remove("visible");
+    document.querySelector('.eitn').classList.remove("visible");
+    document.querySelector('.ninetn').classList.remove("visible");
+    document.querySelector('.twotn').classList.remove("visible");
+    document.querySelector('.onetwotn').classList.remove("visible");
+    document.querySelector('.twotwotn').classList.remove("visible");
+    document.querySelector('.thrtwotn').classList.remove("visible");
+    document.querySelector('.fourtwotn').classList.remove("visible");
+    document.querySelector('.fivetwotn').classList.remove("visible");
+    document.querySelector('.sixtwotn').classList.remove("visible");
+    document.querySelector('.seventntwo').classList.remove("visible");
+    document.querySelector('.eightntwo').classList.remove("visible");
+}
+
+function inproggers(){
+    alert("В разработке!");
 }
